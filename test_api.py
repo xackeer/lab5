21e21ad8dbad9c18a4d2dc5dc7bda4f4efcad109
xackeer <xackeer685@gmail.com>
2025-05-28 13:21:32 +0500

    first commit

--- test_api.py
+++ test_api.py
@@ -2,44 +2,45 @@ import requests
 import pytest
 from jsonschema import validate
 
-#задание 2
-def test_get_users():  
-    response = requests.get("https://reqres.in/api/users?page=2")  
-    assert response.status_code == 200  
-    data = response.json()  
-    assert "data" in data  
+API = "reqres-free-v1"
+HEADER = {
+    "x-api-key": API
+}
+schema = {
+    "type": "object",
+    "properties": {
+        "page": {"type": "number"},
+        "data": {"type": "array"}
+    },
+    "required": ["data"]
+}
 
-schema = {  
-    "type": "object",  
-    "properties": {  
-        "page": {"type": "number"},  
-        "data": {"type": "array"}  
-    }  
-}  
-def test_get_users_schema():  
-    response = requests.get("https://reqres.in/api/users?page=2")  
-    assert validate(response.json(), schema) is None  
+def test_get_users():
+    response = requests.get("https://reqres.in/api/users?page=2", headers= HEADER)
+    assert response.status_code == 200
+    data = response.json()
+    assert "data" in data
 
-#задание 3
-def test_create_user():  
-    payload = {"name": "Alice", "job": "Engineer"}  
-    response = requests.post("https://reqres.in/api/users", json=payload)  
-    assert response.status_code == 201  
-    assert response.json()["id"] is not None  
+def test_get_users_schema():
+    response = requests.get("https://reqres.in/api/users?page=2")
+    validate(instance=response.json(), schema=schema)
 
-@pytest.mark.parametrize("name, job", [("Bob", "QA"), ("Eve", "DevOps")])  
-def test_create_user_params(name, job):  
-    response = requests.post("https://reqres.in/api/users", json={"name": name, "job": job})  
-    assert response.status_code == 201  
+def test_create_user():
+    payload = {"name": "Alice", "job": "Engineer"}
+    response = requests.post("https://reqres.in/api/users", json=payload,  headers= HEADER)
+    assert response.status_code == 201
+    assert response.json().get("id") is not None
 
-#задание 4
-def test_invalid_login():  
-    response = requests.post("https://reqres.in/api/login", json={"email": "test@test"})  
-    assert response.status_code == 400  
-    assert "error" in response.json()  
+@pytest.mark.parametrize("name, job", [("Bob", "QA"), ("Eve", "DevOps")])
+def test_create_user_params(name, job):
+    response = requests.post("https://reqres.in/api/users", json={"name": name, "job": job},  headers= HEADER)
+    assert response.status_code == 201
 
-def test_not_found():  
-    response = requests.get("https://reqres.in/api/users/999")  
-    assert response.status_code == 404  
+def test_invalid_login():
+    response = requests.post("https://reqres.in/api/login", json={"email": "test@test"},  headers= HEADER)
+    assert response.status_code == 400
+    assert "error" in response.json()
 
-
+def test_not_found():
+    response = requests.get("https://reqres.in/api/users/999",  headers= HEADER)
+    assert response.status_code == 404
